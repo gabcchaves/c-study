02adea3c7a50fc244ad2f5b53742b623d85d1b70
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-03-24 19:36:05 +0000

    Exercise 20.

---
+++ VariablesAndExpressions/e20.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	float kg, lbs;
+	printf("Enter a mass value in kilograms:\n");
+	scanf("%f", kg);
+	pounds = kg / 0.45;
+	printf("Mass converted to pounds: %.2f.\n", lbs);
+	return 0;
+}
