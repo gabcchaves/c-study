8d55d4e10f2ada3c2c1bc4a9decb40079a529574
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-06-14 14:09:56 +0000

    Compute the price with increase specified in a table, according to the range of the old price.

--- ConditionalCommands/e33.c
+++ ConditionalCommands/e33.c
@@ -5,14 +5,16 @@ int main() {
 	
 	if (oPrice <= 50) {
 		nPrice = oPrice + oPrice * 0.05;
-		printf("Novo preço: %.2f\nPercentual de aumento: 5%%", nPrice);
 	} else if (oPrice <= 100) {
 		nPrice = oPrice + oPrice * 0.10;
-		printf("Novo preço: %.2f\nPercentual de aumento: 10%%", nPrice);
 	} else {
 		nPrice = oPrice + oPrice * 0.15;
-		printf("Novo preço: %.2f\nPercentual de aumento: 15%%", nPrice);
 	}
+
+	if (nPrice <= 80) puts("Barato");
+	else if (nPrice <= 120) puts("Normal");
+	else if (nPrice <= 200) puts("Caro");
+	else puts("Muito caro");
 
 	return 0;
 }
